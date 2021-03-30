b967473c929bddc111a469808967d78740669e66
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2021-03-29 20:40:21 -0400

    formatting, no substantive change

--- source/Patches/LoanSharks_Repo.cs
+++ source/Patches/LoanSharks_Repo.cs
@@ -22,27 +22,26 @@ namespace LoanSharks
 			{
 			List<MechDef> list = new List<MechDef>(__instance.ActiveMechs.Values);
 			if (__instance.ActiveMechs == null)
-				{
-					return null;
-				}
-				for (int i = 0; i < list.Count; i++)
-				{
-					MechDef mechDef = list[i];
-					if (mechDef != null && string.Compare(mechID, mechDef.Description.Id) == 0) //&& (string.IsNullOrEmpty(uniqueID) || string.Compare(uniqueID, mechDef.GUID) == 0))
-					{
-						return mechDef;
-					}
-				}
-				return null;
-			}
+            {
+                return null;
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                MechDef mechDef = list[i];
+                if (mechDef != null && string.Compare(mechID, mechDef.Description.Id) == 0) //&& (string.IsNullOrEmpty(uniqueID) || string.Compare(uniqueID, mechDef.GUID) == 0))
+                {
+                    return mechDef;
+                }
+            }
+            return null;
+        }
 
-		[HarmonyPatch(typeof(BattleTech.SimGameState), "ApplyEventAction")]
-		public static class ApplyEventAction_Patch
-		{
-			public static void Prefix(SimGameResultAction action, object additionalObject)
-			{
-				
-				{
+        [HarmonyPatch(typeof(BattleTech.SimGameState), "ApplyEventAction")]
+        public static class ApplyEventAction_Patch
+        {
+            public static void Prefix(SimGameResultAction action, object additionalObject)
+            {
+                {
                     if (action.Type == SimGameResultAction.ActionType.Mech_AddRoster)
                     {
                         if (action.additionalValues == null)
@@ -51,81 +50,81 @@ namespace LoanSharks
                         }
                         else
                             ModState.RemoveMechFlag = action.additionalValues.ElementAtOrDefault(0);
-					}
+                    }
                 }
 				
-				return;
-			}
+                return;
+            }
 
-			public static void Postfix(SimGameResultAction action, object additionalObject)
-			{
-				ModState.RemoveMechFlag = "FALSE";
+            public static void Postfix(SimGameResultAction action, object additionalObject)
+            {
+                ModState.RemoveMechFlag = "FALSE";
 
-				return;
-			}
-		}
+                return;
+            }
+        }
 
 
-		[HarmonyPatch(typeof(BattleTech.SimGameState), "AddMechByID")]
-		public static class AddMechByID_Patch
-		{
+        [HarmonyPatch(typeof(BattleTech.SimGameState), "AddMechByID")]
+        public static class AddMechByID_Patch
+        {
 			
-			public static bool Prefix(SimGameState __instance, string id, bool active)
+            public static bool Prefix(SimGameState __instance, string id, bool active)
 			
-			{
-				//SimGameResultAction.;
-				//ModState.RemoveMechFlag = simGameResultAction.additionalValues[0];
-				//sgra.value
-				if (ModState.RemoveMechFlag != "RemoveMech")
-				{ 
-					return true; 
-				}
-				else
-				{
-					MechDef mechDef = GetMechDef(id, __instance);
-					if (__instance.ActiveMechs.ContainsValue(mechDef)==true)
-					{
-						int index = __instance.ActiveMechs.FindFirstKeyByValue(mechDef);
-						if (ModInit.Settings.StoreBeforeRemove == true)
-						{
-							__instance.UnreadyMech(index, mechDef);
-							MechDef mech = new MechDef(__instance.DataManager.MechDefs.Get(id));
-							string MechStatID = "Item.MechDef.";
-							MechStatID += mech.ChassisID;
-							__instance.CompanyStats.ModifyStat<int>("SimGameState", 0, MechStatID, StatCollection.StatOperation.Int_Subtract, 1, -1, true);
-							if (ModInit.Settings.NoNegativeStats == true)
-							{
-								if (__instance.CompanyStats.GetValue<int>(MechStatID) < 0)
-								{
-									__instance.CompanyStats.Set<int>(MechStatID, 0);
-								}
-							}
-						}
-						else
-						{
-							__instance.ActiveMechs.Remove(index);
-						}
-					}
-					else
-					{
-						MechDef mech = new MechDef(__instance.DataManager.MechDefs.Get(id));
-						string MechStatID = "Item.MechDef.";
-						MechStatID += mech.ChassisID;
-						__instance.CompanyStats.ModifyStat<int>("SimGameState", 0, MechStatID, StatCollection.StatOperation.Int_Subtract, 1, -1, true);
-					if (ModInit.Settings.NoNegativeStats == true)
-						{
-							if (__instance.CompanyStats.GetValue<int>(MechStatID) < 0)
-							{
-								__instance.CompanyStats.Set<int>(MechStatID, 0);
-							}
-						}
-					}
+            {
+                //SimGameResultAction.;
+                //ModState.RemoveMechFlag = simGameResultAction.additionalValues[0];
+                //sgra.value
+                if (ModState.RemoveMechFlag != "RemoveMech")
+                { 
+                    return true; 
+                }
+                else
+                {
+                    MechDef mechDef = GetMechDef(id, __instance);
+                    if (__instance.ActiveMechs.ContainsValue(mechDef)==true)
+                    {
+                        int index = __instance.ActiveMechs.FindFirstKeyByValue(mechDef);
+                        if (ModInit.Settings.StoreBeforeRemove == true)
+                        {
+                            __instance.UnreadyMech(index, mechDef);
+                            MechDef mech = new MechDef(__instance.DataManager.MechDefs.Get(id));
+                            string MechStatID = "Item.MechDef.";
+                            MechStatID += mech.ChassisID;
+                            __instance.CompanyStats.ModifyStat<int>("SimGameState", 0, MechStatID, StatCollection.StatOperation.Int_Subtract, 1, -1, true);
+                            if (ModInit.Settings.NoNegativeStats == true)
+                            {
+                                if (__instance.CompanyStats.GetValue<int>(MechStatID) < 0)
+                                {
+                                    __instance.CompanyStats.Set<int>(MechStatID, 0);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            __instance.ActiveMechs.Remove(index);
+                        }
+                    }
+                    else
+                    {
+                        MechDef mech = new MechDef(__instance.DataManager.MechDefs.Get(id));
+                        string MechStatID = "Item.MechDef.";
+                        MechStatID += mech.ChassisID;
+                        __instance.CompanyStats.ModifyStat<int>("SimGameState", 0, MechStatID, StatCollection.StatOperation.Int_Subtract, 1, -1, true);
+                        if (ModInit.Settings.NoNegativeStats == true)
+                        {
+                            if (__instance.CompanyStats.GetValue<int>(MechStatID) < 0)
+                            {
+                                __instance.CompanyStats.Set<int>(MechStatID, 0);
+                            }
+                        }
+                    }
 					
-					return false;
-				}
+                    return false;
+                }
 				
-			}
-		}
+            }
+        }
 	}
 
 }
